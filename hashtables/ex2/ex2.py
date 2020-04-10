4cb93a08ba93d85af8307987fe72e661d22c8668
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-04-10 11:06:16 -0700

    added more comments for readability

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,13 +16,19 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
+    # Initialize the hash table with all ticket nodes
     for ticket in tickets:
         hash_table_insert(hashtable, ticket.source, ticket.destination)
 
+    # Initialize the starting destination to be the source origin of 'NONE'
     destination = 'NONE'
 
+    # Loop through indices
     for index in range(length):
+        # find the next location using the destination as a key
         destination = hash_table_retrieve(hashtable, destination)
+        # add the location to the route list
         route[index] = destination
 
+    # return the route, but chop off the last destination of 'NONE'
     return route[:-1]
